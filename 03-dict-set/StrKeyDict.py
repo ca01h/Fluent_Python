99ccceaa31bd8aa5b33ec6e7d0487ff746a04bcf
caoihuai2 <cc511965738@gmail.com>
2019-07-05 17:50:17 +0800

    Add: 字典相关示例代码

---
+++ 03-dict-set/StrKeyDict.py
@@ -0,0 +1,14 @@
+import collections
+
+class StrDictKey(collections.UserDict):
+
+	def __missing__(self, key):
+		if isinstance(key, str):
+			raise KeyError
+		return self[str(key)]
+
+	def __contains__(self, key):
+		return str(key) in self.data
+
+	def __setitem__(self, key, item):
+		self.data[str(key)] = item
